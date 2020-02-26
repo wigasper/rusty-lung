0853f2ce88e00b4824353ce2ad7b305b1102e16c
William Gasper <wigasper@gmail.com>
2020-02-26 08:51:39 -0600

    reorgnized, started label_prop module skeleton

---
+++ src/label_prop.rs
@@ -0,0 +1,10 @@
+pub mod label_prop {
+    
+    type Node = u64;
+    type Label = u64;
+
+    pub fn label_prop(adj_list: &HashMap<Node, HashSet<Node>>) {
+        let mut node_labels: HashMap<Node, Label> = HashMap::new();
+
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@ pub mod graphbuilder {
         }
 
         if (value + radius) < max {
-            max_bound = value + radius;
+            max_bound = value + radius + 1;
         }
 
         (min_bound, max_bound)
@@ -46,9 +46,10 @@ pub mod graphbuilder {
                         .get_pixel(neighbor_coords.0, neighbor_coords.1)
                         .channels()[0] as i32;
                     
-                    if (node_pixel_val - neighbor_pixel_val).abs() as u64 > threshold {
+                    if ((node_pixel_val - neighbor_pixel_val).abs() as u64) < threshold {
                         let neighbor = nodes_lookup.get(&neighbor_coords).unwrap().to_owned();
                         adj_list.get_mut(&node).unwrap().insert(neighbor);
+                        adj_list.get_mut(&neighbor).unwrap().insert(node.to_owned());
                     }
                 }
             }
@@ -72,6 +73,10 @@ pub mod graphbuilder {
 
         for pixel in img.enumerate_pixels() {
             nodes.insert(node_id, (pixel.0, pixel.1));
+            /////////
+            //let pixel_val = img.get_pixel(pixel.0, pixel.1).channels()[0];
+            //println!("{}: {}, {} - {}", node_id, pixel.0, pixel.1, pixel_val);
+            //////////
             nodes_lookup.insert((pixel.0, pixel.1), node_id);
             adj_list.insert(node_id, HashSet::new());
             node_id += 1;
@@ -98,13 +103,9 @@ mod tests {
     use super::graphbuilder::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-
-    #[test]
     fn test_get_bounds_0() {
-        assert_eq!((0, 7), get_bounds(2, 42, 5))
+        assert_eq!((0, 8), get_bounds(2, 42, 5));
+        assert_eq!((0, 3), get_bounds(0, 3, 2));
     }
 
     #[test]
@@ -115,13 +116,20 @@ mod tests {
     #[test]
     fn test0() {
         //build_graph("/Users/wigasper/repos/bio-segmenter/36pixel.png".to_owned());
-        let adj_list = build_adj_list("/media/storage/bio-segmenter/36pixel.png".to_owned(), 3, 10);
+        //let adj_list = build_adj_list("/media/storage/bio-segmenter/36pixel.png".to_owned(), 3, 10);
+
+    }
+
+    #[test]
+    fn test_build_adj_list() {
+        let adj_list = build_adj_list("../16pixel.png".to_owned(), 2, 30);
         for (key, val) in adj_list.iter() {
             print!("{}: ", key);
             for node in val.iter() {
                 print!("{} ", node);
             }
             println!();
-        }
+        }       
+
     }
 }
