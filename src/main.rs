3a1ac2f0f59a7f04de98d101dc29a44463f2aa6e
William Gasper <wigasper@gmail.com>
2020-03-18 08:27:12 -0500

    added proper arg parser

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,70 @@
-//pub use crate::lib::graphbuilder::*;
-//use bioseg::label_prop::*;
+extern crate clap;
+
 use bioseg::utils::*;
-use std::env;
+
+use clap::{App, Arg};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let fp_in = &args[1];
-    let fp_out = &args[2];
-    let radius: u32 = args[3].parse().unwrap_or_else(|why| {
-        panic!("Could not parse {} to u32", why);
-    });
+    let matches = App::new("bio-segmenter")
+        .version("0.1")
+        .author("William Gasper <wigasper@gmail.com>")
+        .arg(
+            Arg::with_name("INPUT")
+                .short("i")
+                .long("input")
+                .required(true)
+                .takes_value(true)
+                .help("input file path"),
+        )
+        .arg(
+            Arg::with_name("OUTPUT")
+                .short("o")
+                .long("output")
+                .default_value("output.png")
+                .help("output file path"),
+        )
+        .arg(
+            Arg::with_name("RADIUS")
+                .short("r")
+                .long("radius")
+                .default_value("3")
+                .help("initial radius for neighbor checking"),
+        )
+        .arg(
+            Arg::with_name("THRESHOLD")
+                .short("t")
+                .long("threshold")
+                .default_value("5")
+                .help("maximum delta allowed to add an edge between nodes"),
+        )
+        .get_matches();
 
-    let threshold: u8 = args[4].parse().unwrap_or_else(|why| {
-        panic!("Could not parse {} to u8", why);
-    });
+    let input_fp = matches.value_of("INPUT").unwrap();
+    let output_fp = matches.value_of("OUTPUT").unwrap();
 
-    segment_image(fp_in, fp_out, radius, threshold);
-    //let adj_list = build_adj_list(fp.to_owned(), radius, threshold);
+    let radius: u32 = matches
+        .value_of("RADIUS")
+        .unwrap()
+        .parse()
+        .unwrap_or_else(|why| {
+            panic!(
+                "Could not parse '{}' to u32: {}",
+                matches.value_of("RADIUS").unwrap(),
+                why
+            );
+        });
+
+    let threshold: u8 = matches
+        .value_of("THRESHOLD")
+        .unwrap()
+        .parse()
+        .unwrap_or_else(|why| {
+            panic!(
+                "Could not parse '{}' to u8: {}",
+                matches.value_of("THRESHOLD").unwrap(),
+                why
+            );
+        });
+
+    segment_image(input_fp, output_fp, radius, threshold);
 }
